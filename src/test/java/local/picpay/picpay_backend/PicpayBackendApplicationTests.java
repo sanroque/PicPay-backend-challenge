45c4b37b6379d3eb2ea1891151c5ee8f379fecb8
sanroque <goncalo91machado@gmail.com>
2024-09-10 08:48:35 +0100

    feat: initial commit

---
+++ src/test/java/local/picpay/picpay_backend/PicpayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package local.picpay.picpay_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

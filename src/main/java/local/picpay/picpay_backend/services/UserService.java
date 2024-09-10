714e94fb7def5fc6c07160bd147c47ef053c31a8
sanroque <goncalo91machado@gmail.com>
2024-09-10 10:32:39 +0100

    feat: create basic CRUD for user

--- src/main/java/local/picpay/picpay_backend/services/UserService.java
+++ src/main/java/local/picpay/picpay_backend/services/UserService.java
@@ -2,11 +2,13 @@ package local.picpay.picpay_backend.services;
 
 import local.picpay.picpay_backend.domain.user.User;
 import local.picpay.picpay_backend.domain.user.UserType;
+import local.picpay.picpay_backend.dto.UserDTO;
 import local.picpay.picpay_backend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -28,6 +30,16 @@ public class UserService {
         return this.userRepository.findById(id).orElseThrow(() -> new Exception("User not found!"));
     }
 
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        userRepository.save(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+        List<User> users = this.userRepository.findAll();
+        return users;
+    }
     public void saveUser(User user) {
         this.userRepository.save(user);
     }
